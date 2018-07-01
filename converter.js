6816880a8c8cc5d54d01af399d296a0c66364048
austinblizz <austinblizz@gmail.com>
2018-07-01 22:25:31 +0100

    Final work
    Signed-off-by: austinblizz <austinblizz@gmail.com>

--- converter.js
+++ converter.js
@@ -1,165 +1,181 @@
-    /*jshint esversion: 6 */
+//checking for service worker support
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .then(function () {
+            console.log("Service Worker Registered");
+        });
+}
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('sw.js')
-            .then(function () {
-                console.log("Service Worker Registered");
-            });
-    }
+//url for countries
+const url = 'https://free.currencyconverterapi.com/api/v5/countries';
 
-    //url for countries
-    const url = 'https://free.currencyconverterapi.com/api/v5/countries';
+//fetching country currencies for display in the select forms
+fetch(url)
+    .then((response) => response.json()) // Transform the data into json
+    .then((data) => {
+        let currencies = data.results;
 
-    //fetching country currencies for display in the select forms
+        //Saving currencies to idb by calling saving_all_currencies function
+        saving_all_currencies(currencies);
+
+        for (let currency in currencies) { //Loop through the result
+
+            //Getting the currencyId into the option field for *From*
+            let from = document.createElement("option");
+            let selectFrom = document.getElementById("from");
+            selectFrom.appendChild(from);
+            from.innerHTML = `(${currencies[currency].name})` + " " + `(${currencies[currency].currencySymbol})`;
+            from.setAttribute("value", `${currencies[currency].currencyId}`);
+
+            //Getting the currencyId into the option field for *to*
+            let to = document.createElement("option");
+            let selectTo = document.getElementById("to");
+            selectTo.appendChild(to);
+            to.innerHTML = `(${currencies[currency].name})` + " " + `(${currencies[currency].currencySymbol})`;
+            to.setAttribute("value", `${currencies[currency].currencyId}`);
+        }
+    });
+
+//Conversion function
+function convert() {
+    let outputBox = document.getElementById("show"); //Where to display result
+
+    let inputBox = document.getElementById('user_input'); //getting user inputs
+    let user_input = inputBox.value; //passing the value collected from the user to a declared letible
+
+    let select_from = document.getElementById('from'); //getting the user selected currency to convert from        
+    let from = select_from.value; //passing the id which will be used in query
+
+    let select_to = document.getElementById('to'); //getting the user selected currency to convert to
+    let to = select_to.value; //passing the id which will be used in query
+
+    let query = `${from}_${to}`; //preparing a query string
+
+    let url = 'https://free.currencyconverterapi.com/api/v5/convert?q=' + query + '&compact=ultra';
+
+    //fetching converion result from api
     fetch(url)
         .then((response) => response.json()) // Transform the data into json
         .then((data) => {
-            let currencies = data.results;
+            if (isNaN(user_input)) {
+                alert('only Numbers allowed');
+            } else {
+                let output = (user_input * data[query]).toFixed(2); //multiply to get exchange rate and round up to two decimal places
+                console.log(user_input);
 
-            //Saving currencies to idb by calling saving_all_currencies function
-            saving_all_currencies(currencies);
+                //Saving exchange rate by calling saving_exchange_rate function
+                saving_exchange_rate(query, data[query]);
 
-            for (let currency in currencies) { //Loop through the result
-
-                //Getting the currencyId into the option field for *From*
-                let from = document.createElement("option");
-                let selectFrom = document.getElementById("from");
-                selectFrom.appendChild(from);
-                from.innerHTML = currencies[currency].name + " " + `(${currencies[currency].currencySymbol})`;
-                from.setAttribute("value", `${currencies[currency].currencyId}`);
-
-                //Getting the currencyId into the option field for *to*
-                let to = document.createElement("option");
-                let selectTo = document.getElementById("to");
-                selectTo.appendChild(to);
-                to.innerHTML = currencies[currency].name + " " + `(${currencies[currency].currencySymbol})`;
-                to.setAttribute("value", `${currencies[currency].currencyId}`);
+                //Outputting the conversion result for display to the user
+                outputBox.innerHTML = output;
+                outputBox.setAttribute("value", `${output}`);
             }
+        })
+        .catch(() => {
+            return getting_rate_from_cache(query);
+
         });
-    //Conversion function
-    function convert() {
-        let outputBox = document.getElementById("show"); //Where to display result
+}
+//End of Conversion function
 
-        let inputBox = document.getElementById('user_input'); //getting user inputs
-        let user_input = inputBox.value; //passing the value collected from the user to a declared varible
+//getting_rate_from_cache function
+function getting_rate_from_cache(query) {
+    alert('YOU ARE OFFLINE');
+    alert('YOU CAN ONLY VIEW LAST SAVED CONVERSION');
+    console.log('message alerted');
+    let indexedDB = window.indexedDB;
+    let open = indexedDB.open('rates', 1);
+    open.onsuccess = () => {
+        //start a new transaction
+        let db = open.result;
+        let tx = db.transaction('rates', 'readonly');
+        let store = tx.objectStore('rates');
 
-        let select_from = document.getElementById('from'); //getting the user selected currency to convert from        
-        let from = select_from.value; //passing the id which will be used in query
+        console.log('query display');
+        console.log(query);
+        let getRequest = store.index('query').get(query);
 
-        let select_to = document.getElementById('to'); //getting the user selected currency to convert to
-        let to = select_to.value; //passing the id which will be used in query
+        getRequest.onerror = (e) => {
+            console.log('Error', e.target.error.name);
+        };
 
-        let query = from + '_' + to; //preparing a query string
+        getRequest.onsuccess = () => {
+            console.log(getRequest.result);
+        };
+    };
+}
+//End of getting_rate_from_cache function
 
-        let url = 'https://free.currencyconverterapi.com/api/v5/convert?q=' + query + '&compact=ultra';
+//saving_exchange_rate function
+function saving_exchange_rate(query, id) {
 
-        //fetching converion result from api
-        fetch(url)
-            .then((response) => response.json()) // Transform the data into json
-            .then((data) => {
-                if (isNaN(user_input)) {
-                    alert('Numbers allowed only');
-                } else {
-                    let output = (user_input * data[query]).toFixed(2); //multiply to get exchange rate and round up to two decimal places
-                    console.log(user_input);
+    //creating or opening database
+    if (navigator.serviceWorker) {
+        let indexedDB = window.indexedDB;
+        let open = indexedDB.open('rates', 1);
 
-                    //Saving exchange rate by calling saving_exchange_rate function
-                    saving_exchange_rate(data[query], query);
-
-                    //Outputting the conversion result for display to the user
-                    outputBox.innerHTML = output;
-                    outputBox.setAttribute("value", `${output}`);
-                }
-            })
-            .catch(() => {
-                alert('YOU ARE OFFLINE');
-                let indexedDB = window.indexedDB;
-                let open = indexedDB.open('rates', 1);
-                open.onsuccess = function () {
-                    //start a new transaction
-                    let db = open.result;
-                    var transaction = db.transaction('rates', 'readonly');
-                    var objectStore = transaction.objectStore('rates');
-
-                    var myIndex = objectStore.index('query');
-                    console.log('query display');
-                    console.log(query);
-                    var getRequest = myIndex.get(query);
-                    getRequest.onsuccess = () => {
-                        console.log(getRequest.result);
-                    };
-                };
+        //creating schema
+        open.onupgradeneeded = () => {
+            let db = open.result;
+            let store = db.createObjectStore("rates", {
+                keypath: "query"
             });
-    }
-
-    function saving_exchange_rate(query, id) {
-        //creating or opening database
-        if (navigator.serviceWorker) {
-            let indexedDB = window.indexedDB;
-            let open = indexedDB.open('rates', 1);
-            //creating schema
-            open.onupgradeneeded = () => {
+            store.createIndex(query, 'query');
+            open.onsuccess = () => {
+                //start a new transaction
                 let db = open.result;
-                let store = db.createObjectStore("rates", {
-                    keypath: "query"
-                });
-                store.createIndex(query, 'query');
-                console.log('here');
-                open.onsuccess = () => {
-                    //start a new transaction
-                    let db = open.result;
-                    let tx = db.transaction('rates', 'readwrite');
-                    console.log(' saving exchange here 1');
-                    let store = tx.objectStore('rates');
-                    console.log('saving exchange here 2');
-                    //add some data               
-                    store.add(query, id);
+                let tx = db.transaction('rates', 'readwrite');
+                let store = tx.objectStore('rates');
 
-                    tx.oncomplete = () => {
-                        console.log('saving exchange here 3');
-                        db.close();
-                    };
+                //add some data               
+                store.add(query, id);
+
+                tx.oncomplete = () => {
+                    console.log('saving exchange here 3');
+                    db.close();
                 };
             };
-        } else {
-            console.log('No Service Wroker Function with this browser');
-        }
+        };
+    } else {
+        console.log('No Service Wroker Function with this browser');
     }
+}
+//End of saving_exchange_rate function
 
-    function saving_all_currencies(currencies) {
-        //creating or opening database
-        if (navigator.serviceWorker) {
-            let indexedDB = window.indexedDB;
-            let open = indexedDB.open('currencies', 1);
-            //creating schema
-            open.onupgradeneeded = () => {
+//saving_all_currencies function
+function saving_all_currencies(currencies) {
+
+    //creating or opening database
+    if (navigator.serviceWorker) {
+        let indexedDB = window.indexedDB;
+        let open = indexedDB.open('currencies', 1);
+
+        //creating schema
+        open.onupgradeneeded = () => {
+            let db = open.result;
+            let store = db.createObjectStore("currencies", {
+                keypath: "id"
+            });
+            let index = store.createIndex('by-date', 'time');
+            open.onsuccess = () => {
+
+                //start a new transaction
                 let db = open.result;
-                let store = db.createObjectStore("currencies", {
-                    keypath: "id"
-                });
-                let index = store.createIndex('by-date', 'time');
-                console.log('here');
-                open.onsuccess = () => {
-                    //start a new transaction
-                    let db = open.result;
-                    let tx = db.transaction('currencies', 'readwrite');
-                    console.log('here 1');
-                    let store = tx.objectStore('currencies');
-                    console.log('here 2');
-                    // let index = store.index('by-date');
-                    //add some data
-                    //store.put('12');               
-                    for (const currency in currencies) {
-                        store.put(currencies[currency].name, currencies[currency].id);
-                    }
-                    tx.oncomplete = () => {
-                        console.log('here 7');
-                        db.close();
-                    };
+                let tx = db.transaction('currencies', 'readwrite');
+                let store = tx.objectStore('currencies');
+
+                //add some data        
+                for (const currency in currencies) {
+                    store.put(currencies[currency].name, currencies[currency].id);
+                }
+                tx.oncomplete = () => {
+                    db.close();
                 };
             };
-        } else {
-            console.log('No Service Wroker Function with this browser');
-        }
+        };
+    } else {
+        console.log('No Service Wroker Function with this browser');
     }
+}
+//End of saving_all_currencies function
